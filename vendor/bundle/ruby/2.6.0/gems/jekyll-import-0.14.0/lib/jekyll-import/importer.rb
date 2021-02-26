169fc5f96a8ac5fd89ff7483230de9a26d37dea6
Robert Curth <robert@rocu.de>
2021-02-26 11:35:49 +0100

    Add new version

---
+++ vendor/bundle/ruby/2.6.0/gems/jekyll-import-0.14.0/lib/jekyll-import/importer.rb
@@ -0,0 +1,26 @@
+module JekyllImport
+  class Importer
+    def self.inherited(base)
+      subclasses << base
+    end
+
+    def self.subclasses
+      @subclasses ||= []
+    end
+
+    def self.stringify_keys(hash)
+      the_hash = hash.clone
+      the_hash.keys.each do |key|
+        the_hash[(key.to_s rescue key) || key] = the_hash.delete(key)
+      end
+      the_hash
+    end
+
+    def self.run(options = {})
+      opts = stringify_keys(options)
+      self.require_deps
+      self.validate(opts) if self.respond_to?(:validate)
+      self.process(opts)
+    end
+  end
+end
